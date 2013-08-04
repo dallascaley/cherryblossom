04a55e1ff2ec592d9638e0b19736a7ead1308b85
Dallas <dallascaley@gmail.com>
2013-08-04 14:26:45 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Cherry Blossom Development</title>
+</head>
+<body>
+<h2>Welcome to the future home of Cherry Blossom Development... </h2>
+<i>yet another bunch of guys trying to make money in software development</i>
+<span>... messin around</span>
+<h2> Hello World!</h2>
+<p>And now for an image:</p>
+<img src="http://daps.tv/wp-content/uploads/2012/02/dr-zaius-10.jpeg" Dr. Zaius align="bottom"/>
+</body>
+</html>
