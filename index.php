c51aa27957fbfefd966f48be58bb27503e46f9be
Dallas Caley <dallas.caley@corp.updatepromise.com>
2018-11-06 16:07:12 +0000

    Palette consideration

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <html>
 <head>
-<title>Cherry Blossom Development</title>
+    <title>Cherry Blossom Development</title>
+    <style>
+        .palette span {
+            padding: 10px;
+        }
+        .light-crimson { background-color: #F76F8E; }
+        .granite-gray { background-color: #686868; }
+        .rackley { background-color: #6A8EAE; }
+        .pistacio { background-color: #95BF74; }
+        .pale-robin-egg-blue { background-color: #99DDC8; }
+    </style>
 </head>
 <body>
-<h2>Welcome to the future home of Cherry Blossom Development... </h2>
-<i>yet another bunch of guys trying to make money in software development</i>
-<span>... messin around</span>
-<h2> Hello World!</h2>
-<p>And now for an image:</p>
-<img src="http://daps.tv/wp-content/uploads/2012/02/dr-zaius-10.jpeg" Dr. Zaius align="bottom"/>
+    <h2>Welcome to the future home of Cherry Blossom Development... </h2>
+    <i>yet another bunch of guys trying to make money in software development</i>
+    <span>... messin around</span>
+    <h2> Hello World!</h2>
+    <p>And now for an image:</p>
+    <img src="http://www.everintransit.com/wp-content/uploads/2016/04/cherry-blossoms-tokyo-ueno-park.jpg"/>
+
+    <div class="palette">
+        <span class="light-crimson">Light Crimson</span>
+        <span class="granite-gray">Granite Grey</span>
+        <span class="rackley">Rackley</span>
+        <span class="pistacio">Pistacio</span>
+        <span class="pale-robin-egg-blue">Pale Robin Egg Blue</span>
+    </div>
 </body>
 </html>
